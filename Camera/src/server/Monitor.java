cd475a6842c0b349e4faae22613739ad9b595478
Kasper Socha <fte10kso@student.lu.se>
2015-11-15 15:04:06 +0100

    Initial idea for OutputHandler and corresponding Monitor methods

--- Camera/src/server/Monitor.java
+++ Camera/src/server/Monitor.java
@@ -1,11 +1,12 @@
 package server;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.net.Socket;
 
 public class Monitor {
 	private Socket client;
 	private ClientPackage toSend;
-	
 
 	public synchronized void setSocket(Socket client) {
 		this.client = client;
@@ -21,14 +22,58 @@ public class Monitor {
 			}
 		}
 	}
-	
+
+	public synchronized void waitForConnection() {
+		while (!isConnected()) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
 	public synchronized boolean isConnected() {
 		return client != null;
+	}
+
+	public synchronized void disconnect() {
+		try {
+			client.close();
+		} catch (Exception e) {
+		}
+		client = null;
+		notifyAll();
 	}
 
 	public synchronized void addPackage(ClientPackage clientPackage) {
 		toSend = clientPackage;
 		notifyAll();
+	}
+
+	public synchronized OutputStream getOutputStream() {
+		if (isConnected()) {
+			try {
+				return client.getOutputStream();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+
+	public synchronized ClientPackage getNextPackage() {
+		while (toSend == null) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		ClientPackage toReturn = toSend;
+		toSend = null;
+		return toReturn;
 	}
 
 }
