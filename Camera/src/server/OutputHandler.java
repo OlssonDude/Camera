b18892d77dee7398a20656cee2221da51240ac39
Kasper Socha <fte10kso@student.lu.se>
2015-11-15 14:36:16 +0100

    Initial idea for OutputHandler and corresponding Monitor methods

--- Camera/src/server/OutputHandler.java
+++ Camera/src/server/OutputHandler.java
@@ -1,9 +1,32 @@
 package server;
 
+import java.io.IOException;
+import java.io.OutputStream;
+
 public class OutputHandler extends Thread {
 	private Monitor monitor;
+	private OutputStream out;
 
 	public OutputHandler(Monitor monitor) {
 		this.monitor = monitor;
+	}
+
+	@Override
+	public void run() {
+		monitor.waitForConnection();
+		out = monitor.getOutputStream();
+		while (true) {
+			if (monitor.isConnected()) {
+				ClientPackage toSend = monitor.getNextPackage();
+				try {
+					out.write(toSend.toByteArray());
+				} catch (IOException e) {
+					monitor.disconnect();
+				}
+			} else {
+				monitor.waitForConnection();
+				out = monitor.getOutputStream();
+			}
+		}
 	}
 }
