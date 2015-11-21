50fb5149dce598f65a26b8021a15b55c69db8ca3
Kasper Socha <fte10kso@student.lu.se>
2015-11-21 13:37:57 +0100

    Implemented functionality for adding and retrieving images.

--- Camera/src/client/ImageBuffer.java
+++ Camera/src/client/ImageBuffer.java
@@ -1,5 +1,28 @@
 package client;
 
+import java.util.PriorityQueue;
+
 public class ImageBuffer {
+	PriorityQueue<Image> buffer;
+	
+	public ImageBuffer() {
+		buffer = new PriorityQueue<Image>();
+	}
+	
+	public synchronized Image getNextImage() {
+		while(buffer.isEmpty()) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return buffer.poll();
+	}
+	
+	public synchronized void addImage(Image image) {
+		buffer.add(image);
+	}
 
 }
