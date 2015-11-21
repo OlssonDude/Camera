2d8ff918aee3bb02dc8098f264dfd1d4ba0d55fa
Kasper Socha <fte10kso@student.lu.se>
2015-11-21 13:27:42 +0100

    Initial Image
    Fields and constructor from uml

--- Camera/src/client/Image.java
+++ Camera/src/client/Image.java
@@ -1,5 +1,15 @@
 package client;
 
 public class Image {
+	private int cameraID;
+	private byte[] jpeg;
+	private long timestamp;
+
+	public Image(int cameraID, long timestamp, byte[] jpeg) {
+		super();
+		this.cameraID = cameraID;
+		this.jpeg = jpeg;
+		this.timestamp = timestamp;
+	}
 
 }
