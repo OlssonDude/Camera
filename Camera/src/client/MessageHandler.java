e77e519bb169e08df8e6d187257b167f7e03aaaf
Kasper Socha <fte10kso@student.lu.se>
2015-11-23 21:41:24 +0100

    streams renamed for consistency

--- Camera/src/client/MessageHandler.java
+++ Camera/src/client/MessageHandler.java
@@ -5,13 +5,13 @@ import java.io.OutputStream;
 
 public class MessageHandler extends Thread {
 	private MessageBuffer buffer;
+	private OutputStream out0;
 	private OutputStream out1;
-	private OutputStream out2;
 
-	public MessageHandler(MessageBuffer buffer, OutputStream out1, OutputStream out2) {
+	public MessageHandler(MessageBuffer buffer, OutputStream out0, OutputStream out1) {
 		this.buffer = buffer;
+		this.out0 = out0;
 		this.out1 = out1;
-		this.out2 = out2;
 	}
 
 	@Override
@@ -20,14 +20,14 @@ public class MessageHandler extends Thread {
 			ServerMessage message = buffer.getMessage();
 			if (message.getId() == 1) {
 				try {
-					out1.write(message.getMessage());
+					out0.write(message.getMessage());
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			} else {
 				try {
-					out2.write(message.getMessage());
+					out1.write(message.getMessage());
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
