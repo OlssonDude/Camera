552b5081e8009f55abe855f5b928b80619b2a417
Kasper Socha <fte10kso@student.lu.se>
2015-11-23 21:25:23 +0100

    Initial implementation from UML

--- Camera/src/client/ServerMessage.java
+++ Camera/src/client/ServerMessage.java
@@ -1,5 +1,23 @@
 package client;
 
 public class ServerMessage {
+	public static final byte IDLE_MESSAGE = 0;
+	public static final byte MOVIE_MESSAGE = 1;
+	public static final byte DISCONNECT_MESSAGE = 2;
+	
+	private int id;
+	private byte message;
+	
+	public ServerMessage(int id, byte message) {
+		this.id = id;
+		this.message = message;
+	}
 
+	public int getId() {
+		return id;
+	}
+
+	public byte getMessage() {
+		return message;
+	}
 }
