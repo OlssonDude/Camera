5471e6472c792baec554abb3812c8de84282ed63
Kasper Socha <fte10kso@student.lu.se>
2015-11-23 21:32:09 +0100

    Initial implementation from UML

--- Camera/src/client/MessageBuffer.java
+++ Camera/src/client/MessageBuffer.java
@@ -1,5 +1,29 @@
 package client;
 
-public class MessageBuffer {
+import java.util.ArrayDeque;
+import java.util.Queue;
 
+public class MessageBuffer {
+	private Queue<ServerMessage> buffer;
+
+	public MessageBuffer() {
+		buffer = new ArrayDeque<ServerMessage>();
+	}
+	
+	public synchronized void addMessage(ServerMessage message) {
+		buffer.offer(message);
+		notifyAll();
+	}
+	
+	public synchronized ServerMessage getMessage() {
+		while(buffer.isEmpty()) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return buffer.poll();
+	}
 }
