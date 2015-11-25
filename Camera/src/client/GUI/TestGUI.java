7da9045a29abe57ff85c40a6739e69b174e67808
Kasper Socha <fte10kso@student.lu.se>
2015-11-25 13:08:51 +0100

    Removed old helper methods

--- Camera/src/client/GUI/TestGUI.java
+++ Camera/src/client/GUI/TestGUI.java
@@ -13,29 +13,13 @@ public class TestGUI extends JFrame {
 
 		setLayout(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
-		c.insets = new Insets(5, 5, 5, 5);
-		addComponent(c, cameraLeft, 0, 0, 1, 3, GridBagConstraints.NONE, 0, 0);
-		addComponent(c, cameraRight, 1, 0, 1, 3, GridBagConstraints.NONE, 0, 0);
+		c.insets = new Insets(2, 2, 2, 2);
+		add(cameraLeft, c);
+		c.gridx = 1;
+		add(cameraRight, c);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		pack();
 		setLocationRelativeTo(null);
 		setVisible(true);
 	}
-
-	private void addComponent(GridBagConstraints c, JComponent component, int x, int y) {
-		addComponent(c, component, x, y, 1, 1, GridBagConstraints.NONE, 0, 0);
-	}
-
-	private void addComponent(GridBagConstraints c, JComponent component, int x, int y, int width, int height, int fill,
-			double wx, double wy) {
-		c.gridx = x;
-		c.gridy = y;
-		c.gridwidth = width;
-		c.gridheight = height;
-		c.fill = fill;
-		c.weightx = wx;
-		c.weighty = wy;
-		add(component, c);
-	}
-
 }
